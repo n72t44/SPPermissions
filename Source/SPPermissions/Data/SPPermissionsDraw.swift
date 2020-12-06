ee3246d0a9c6c641179cf59a783f8be7f654b0d5
n72t44 <n72t44@users.noreply.github.com>
2020-12-06 20:30:39 +0100

    Update SPPermissionsDraw.swift

--- Source/SPPermissions/Data/SPPermissionsDraw.swift
+++ Source/SPPermissions/Data/SPPermissionsDraw.swift
@@ -564,7 +564,7 @@ class SPPermissionsDraw: NSObject {
         
         context.saveGState()
         let resizedFrame: CGRect = resizing.apply(rect: CGRect(x: 0, y: 0, width: 450, height: 450), target: targetFrame)
-        context.translateBy(x: resizedFrame.minX+7, y: resizedFrame.minY)
+        context.translateBy(x: resizedFrame.minX+7, y: resizedFrame.minY+10)
         context.scaleBy(x: resizedFrame.width / 450, y: resizedFrame.height / 450)
 
         let path = UIBezierPath()
